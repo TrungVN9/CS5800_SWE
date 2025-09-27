053a5cdf0d050e2ad36f4807efd5d10936fdffa9
TrungVN9 <trungnvong956@g.ucla.edu>
2025-09-26 18:27:58 -0700

    working in progress hw3 part 2

---
+++ hw3/Customer.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author trungvong
+ */
+public class Customer {
+    private String name;
+    private Macronutrient macro;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+}
---
+++ hw3/Macronutrient.java
@@ -0,0 +1,7 @@
+/*
+ *
+ * @author trungvong
+ */
+public interface Macronutrient {
+      
+}
