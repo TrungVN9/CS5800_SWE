a0aad0e1282c00da9a80c2a016889001ea252c58
TrungVN9 <trungnvong956@g.ucla.edu>
2025-10-03 20:31:22 -0700

    update

--- hw3/MacronutrientFactory.java
+++ hw3/MacronutrientFactory.java
@@ -9,8 +9,6 @@ package hw3;
 public abstract class MacronutrientFactory {
     abstract Carbs createCarbs();
     abstract Protein createProtein();
-
     abstract Fats createFats();
-    
     public abstract String getPlanName();
 }
