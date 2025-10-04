08ebd3994151b4ed9c86d7771578f7a8b9bba060
TrungVN9 <trungnvong956@g.ucla.edu>
2025-10-04 14:26:46 -0700

    updated hw3 completion

--- hw3/Question2.java
+++ hw3/Question2.java
@@ -6,22 +6,28 @@ package hw3;
 public class Question2 {
     public static void main(String[] args) {
         MacronutrientFactory noRestrictionOption = NoRestrictionFactory.getInstance();
-        MacronutrientFactory paleoOption = PaleoFactory.getInstance();
-        MacronutrientFactory veganOption = VeganFactory.getInstance();
-        MacronutrientFactory nutAllergyOption = NutAllergyFactory.getInstance();
-
-        Customer tom = new Customer("Tom", nutAllergyOption);
-        Customer john = new Customer("John", veganOption);
         Customer cherry = new Customer("Cherry", noRestrictionOption);
-        Customer victor = new Customer("Victor", paleoOption);
-        Customer hana = new Customer("Hana", nutAllergyOption);
         Customer peter = new Customer("Peter", noRestrictionOption);
         
-        tom.selectDietPlan();
-        john.selectDietPlan();
         cherry.selectDietPlan();
-        victor.selectDietPlan();
-        hana.selectDietPlan();
         peter.selectDietPlan();
+    
+        MacronutrientFactory paleoOption = PaleoFactory.getInstance();
+        Customer victor = new Customer("Victor", paleoOption);
+        
+        victor.selectDietPlan();
+        
+        MacronutrientFactory veganOption = VeganFactory.getInstance();
+        Customer john = new Customer("John", veganOption);
+        
+        john.selectDietPlan();
+        
+        MacronutrientFactory nutAllergyOption = NutAllergyFactory.getInstance();
+        Customer tom = new Customer("Tom", nutAllergyOption);
+        Customer hana = new Customer("Hana", nutAllergyOption);
+        
+        tom.selectDietPlan();
+        hana.selectDietPlan();
+        
     }
 }
