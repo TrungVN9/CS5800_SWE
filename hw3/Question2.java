b49a5157c0e6977419d72508fee5c08bae9a99d8
TrungVN9 <trungnvong956@g.ucla.edu>
2025-10-06 07:55:08 -0700

    temp commit

--- hw3/Question2.java
+++ hw3/Question2.java
@@ -6,22 +6,28 @@ package hw3;
 public class Question2 {
     public static void main(String[] args) {
         MacronutrientFactory noRestrictionOption = new NoRestrictionFactory();
-        MacronutrientFactory paleoOption = new PaleoFactory();
-        MacronutrientFactory veganOption = new VeganFactory();
-        MacronutrientFactory nutAllergyOption = new NutAllergyFactory();
-
-        Customer tom = new Customer("Tom", nutAllergyOption);
-        Customer john = new Customer("John", veganOption);
         Customer cherry = new Customer("Cherry", noRestrictionOption);
-        Customer victor = new Customer("Victor", paleoOption);
-        Customer hana = new Customer("Hana", nutAllergyOption);
         Customer peter = new Customer("Peter", noRestrictionOption);
-        
-        tom.selectDietPlan();
-        john.selectDietPlan();
+
         cherry.selectDietPlan();
-        victor.selectDietPlan();
-        hana.selectDietPlan();
         peter.selectDietPlan();
+
+        MacronutrientFactory nutAllergyOption = new NutAllergyFactory();
+        Customer tom = new Customer("Tom", nutAllergyOption);
+        Customer hana = new Customer("Hana", nutAllergyOption);
+
+        tom.selectDietPlan();
+        hana.selectDietPlan();
+
+        MacronutrientFactory veganOption = new VeganFactory();
+        Customer john = new Customer("John", veganOption);
+        
+        john.selectDietPlan();
+
+        MacronutrientFactory paleoOption = new PaleoFactory();
+        Customer victor = new Customer("Victor", paleoOption);
+        
+        victor.selectDietPlan();
+        
     }
 }
